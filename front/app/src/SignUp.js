97c12b2da41254aeeaa1a210b77613b834aab8ea
Nibor51 <jonval.robin@gmail.com>
2022-03-22 15:02:50 +0100

    changed indexation

--- front/app/src/SignUp.js
+++ front/app/src/SignUp.js
@@ -8,7 +8,7 @@ export default function SignUp() {
     name: '',
     lastname: '',
   });
-  const myJSON = JSON.stringify(form,1,1);
+  const myJSON = JSON.stringify(form, 1, 1);
 
   function updateField(e) {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -21,52 +21,52 @@ export default function SignUp() {
 
   return (
     <>
-        <h1>
-            {myJSON}
-        </h1>
-        <form onSubmit={handleSubmit}>
-          <label>
-              Email: 
-              <input
-                name="email"
-                value={form.email}
-                onChange={updateField}
-              />
-          </label>
-          <label>
-              Password:
-              <input
-                name="password"
-                value={form.password}
-                onChange={updateField}
-              />
-          </label>
-          <label>
-              Confirm Password:
-              <input
-                name="confirmPassword"
-                value={form.confirmPassword}
-                onChange={updateField}
-              />
-          </label>
-          <label>
-              Name:
-              <input
-                name="name"
-                value={form.name}
-                onChange={updateField}
-              />
-          </label>
-          <label>
-              Lastname:
-              <input
-                name="lastname"
-                value={form.lastname}
-                onChange={updateField}
-              />
-          </label>
-          <button type="submit">Sign Up</button>
-        </form>
+      <h1>
+        {myJSON}
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input
+            name="email"
+            value={form.email}
+            onChange={updateField}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            name="password"
+            value={form.password}
+            onChange={updateField}
+          />
+        </label>
+        <label>
+          Confirm Password:
+          <input
+            name="confirmPassword"
+            value={form.confirmPassword}
+            onChange={updateField}
+          />
+        </label>
+        <label>
+          Name:
+          <input
+            name="name"
+            value={form.name}
+            onChange={updateField}
+          />
+        </label>
+        <label>
+          Lastname:
+          <input
+            name="lastname"
+            value={form.lastname}
+            onChange={updateField}
+          />
+        </label>
+        <button type="submit">Sign Up</button>
+      </form>
     </>
   );
 }
